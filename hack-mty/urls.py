7843538d532738a7be1474f1f2c182dead066b37
Ali Ghahraei Figueroa <aligf94@gmail.com>
2016-08-28 00:05:21 -0500

    Reorder regexps -.-

--- hack-mty/urls.py
+++ hack-mty/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^login/', include('client-auth.urls', namespace="client-auth")),
-    url(r'', include('ratings.urls')),
     url(r'^emotion/', include('emotion.urls', namespace="emotion")),
+    url(r'', include('ratings.urls')),
 ]
