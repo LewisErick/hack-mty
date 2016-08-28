9ee62978700e1675d5dee28ddab2b652a787ec42
lagwy <lagwyf@gmail.com>
2016-08-27 20:56:26 -0500

    Card updated

--- assets/js/card.js
+++ assets/js/card.js
@@ -4,8 +4,9 @@
 var descripciones = document.getElementsByClassName("lib-desc");
 for (var i = 0; i < descripciones.length; i++){
     var desc = descripciones[i].innerHTML;
+    var newDesc;
     if (desc.length >= 100){
-        var newDesc = desc.substr(0,96) + "...";
+        newDesc = desc.substr(0,96) + "...";
         descripciones[i].innerHTML = newDesc;
     } else {
 
